63f6350a219f0da3e64e2be0bd90d6b39857da6d
Sergey Lunyaka <ls@hookprod.com>
2026-02-05 16:55:58 +0300

    и снова седая ночь

--- back/src/files/file-storage.service.ts
+++ back/src/files/file-storage.service.ts
@@ -51,16 +51,7 @@ export class FileStorageService {
       }),
     );
 
-    let url = await this.getSignedUrl(key);
-
-    // Подписанный URL приходит с внутренним hostname (minio:9000).
-    // Для браузера снаружи подменяем его на внешний адрес сервера, если задан.
-    const publicHost =
-      this.config.get<string>('S3_PUBLIC_HOST') ?? 'http://213.226.126.196:9000';
-    if (publicHost) {
-      url = url.replace('http://minio:9000', publicHost.replace(/\/+$/, ''));
-    }
-
+    const url = await this.getSignedUrl(key);
     return { bucket: this.bucket, key, url };
   }
 
